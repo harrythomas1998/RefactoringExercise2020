99e6fae5c9279cbf8bd428630ed97538fe7e1884
harrythomas <c16455964@mytudublin.ie>
2020-03-06 14:56:31 +0000

    Added MenuInterface

---
+++ src/MenuInterface.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public interface MenuInterface {
+
+     ArrayList<Customer> customerList = new ArrayList<>();
+     ArrayList<CustomerAccount> accounts = new ArrayList<> ();
+     ArrayList<AccountTransaction> transactionList = new ArrayList<>();
+
+}
